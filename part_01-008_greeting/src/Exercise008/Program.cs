b3753b16b76fbe031a9b88bfde3cb5f9f126413b
Marionum <105773495+marionum@users.noreply.github.com>
2023-11-03 10:59:10 +0200

    Add files via upload

---
+++ part_01-008_greeting/src/Exercise008/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise008
+{
+    using System;
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+             Console.WriteLine("What is your name?");
+            string input = Console.ReadLine();
+            string greeting = "Hello ";
+            Console.WriteLine(greeting + input + "!");
+
+
+        }
+    }
+}
---
+++ part_01-009_conversation/src/Exercise009/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Exercise009
+{
+  using System;
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      
+      Console.Write("Hello, how are you?" + "\n" );
+      string message = Console.ReadLine();
+      Console.Write("That's interesting, tell me more" + "\n");
+      string message2 = Console.ReadLine();
+      Console.WriteLine ("Thank you for sharing" + "!");
+      
+     
+
+    }
+  }
+}
