b3b14ff4c54a4184a2f016d5eef8917eb72f6a60
Marionum <105773495+marionum@users.noreply.github.com>
2023-11-03 11:00:38 +0200

    Add files via upload

---
+++ part_01-010_name_profession/src/Exercise010/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Exercise010
+{
+  using System;
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+
+      Console.Write("I will tell a story, but I need some information.\nGive a name for main character:\n");
+      string name = Console.ReadLine();
+    
+      Console.Write("Give the character a profession:\n");
+      string profession = Console.ReadLine();
+
+      Console.Write("Here is the story:\n");
+
+      
+      Console. WriteLine ("Once upon a time there was a " + profession + " called " + name);
+      Console.WriteLine ("On her way to work, " + name + " often pondered what being " + profession + " meant to them.");
+      Console.WriteLine ("When you work as a " + profession + " you meet interesting people.");
+      Console.WriteLine (name + " enjoys their work as " + profession + "," + " The end.");
+  
+     
+
+
+
+    }
+  }
+}
---
+++ part_01-012_print_integer/src/Exercise012/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise012
+{
+  using System;
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Give a number!");
+       string message = Console.ReadLine();
+
+      Console.WriteLine("You gave " + message);
+
+    }
+  }
+}
